993de1883e9b69ff8abca054821f105958140bac
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-10-21 21:14:12 +0330

    update score and show next question

--- js/game.js
+++ js/game.js
@@ -4,13 +4,19 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
+const nextButton = document.getElementById("next-button");
+const questionNumber = document.getElementById("question-number");
 
+const CORRECT_BONUS = 10;
 const URL =
   "https://opentdb.com/api.php/?amount=10&difficulty=easy&type=multiple";
 
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -26,6 +32,7 @@ const start = () => {
 };
 
 const showQuestion = () => {
+  questionNumber.innerText = questionIndex + 1;
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
   correctAnswer = correctAnswerIndex;
@@ -36,16 +43,39 @@ const showQuestion = () => {
 };
 
 const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
+
   const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
   } else {
     event.target.classList.add("incorrect");
-    answerList[correctAnswer].classList.add("correct")
+    answerList[correctAnswer].classList.add("correct");
   }
 };
 
+const nextHandler = () => {
+  questionIndex++;
+  if (questionIndex < formattedData.length) {
+    isAccepted = true;
+    removeClasses();
+    showQuestion();
+  } else {
+    window.location.assign("end.html");
+  }
+};
+
+const removeClasses = () => {
+  answerList.forEach((button) => {
+    button.className = "answer-text";
+  });
+};
+
 window.addEventListener("load", fetchData);
+nextButton.addEventListener("click", nextHandler);
 answerList.forEach((button, index) => {
   button.addEventListener("click", (event) => checkAnswer(event, index));
 });
